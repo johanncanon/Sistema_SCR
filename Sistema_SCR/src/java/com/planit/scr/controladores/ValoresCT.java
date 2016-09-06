c2fcbe479c255012360a2f633cbabbd5868533d5
Scordevil <Cardenasg66@gmail.com>
2016-09-06 16:22:14 -0500

    arreglos varios

--- Sistema_SCR/src/java/com/planit/scr/controladores/ValoresCT.java
+++ Sistema_SCR/src/java/com/planit/scr/controladores/ValoresCT.java
@@ -64,25 +64,28 @@ public class ValoresCT {
     }
 
     public void registrarValores() throws Exception {
-
         Calendar C = Calendar.getInstance();
-        int sAnio = C.get(Calendar.YEAR);
         double vt = ((valor.getV1() + valor.getV2()));
 
         try {
             try {
-                String sql = "INSERT INTO public.valores (px, pf, v1, v2, trimestre, vt, ctmd, cmt, ctmc, cr, cce, ctme)"
-                        + " VALUES(" + valor.getPx() + "," + valor.getPf() + ","
+                String sql = "INSERT INTO public.valores (px, pf, v1, v2, trimestre, vt, ctmd, cmt, ctmc, cr, cce, ctme, anio)"
+                        + " VALUES(" + valor.getPx() + ","
+                        + " " + valor.getPf() + ","
                         + " " + valor.getV1() + ","
                         + " " + valor.getV2() + ","
-                        + " 'Trimestre " + valor.getTrimestre() + " del " + sAnio + " ', " + vt + ","
+                        + " 'Trimestre " + valor.getTrimestre() + "',"
+                        + " " + valor.getVt() + ","
                         + " " + valor.getCtmd() + ","
                         + " " + valor.getCmt() + ","
                         + " " + valor.getCtmc() + ","
                         + " " + valor.getCr() + ","
                         + " " + valor.getCce() + ","
-                        + " " + valor.getCtme() + ")";
+                        + " " + valor.getCtme() + ","
+                        + " '" + valor.getAnio() + "')";
+
                 st.execute(sql);
+
             } catch (SQLException e) {
                 throw e;
             }
@@ -99,11 +102,11 @@ public class ValoresCT {
         List<Valores> listaValores = new ArrayList<>();
         try {
             try {
-                String sql = "SELECT idvalores, px, pf, v1, v2, trimestre, vt, ctmd, cmt, ctmc, cr, cce, ctme "
+                String sql = "SELECT idvalores, px, pf, v1, v2, trimestre, vt, ctmd, cmt, ctmc, cr, cce, ctme, anio "
                         + "  FROM public.valores;";
                 ResultSet rs = st.executeQuery(sql);
                 while (rs.next()) {
-                    listaValores.add(new Valores(rs.getInt(1), rs.getDouble(2), rs.getDouble(3), rs.getDouble(4), rs.getDouble(5), rs.getString(6), rs.getDouble(7), rs.getDouble(8), rs.getDouble(9), rs.getDouble(10), rs.getDouble(11), rs.getDouble(12), rs.getDouble(13)));
+                    listaValores.add(new Valores(rs.getInt(1), rs.getDouble(2), rs.getDouble(3), rs.getDouble(4), rs.getDouble(5), rs.getString(6), rs.getDouble(7), rs.getDouble(8), rs.getDouble(9), rs.getDouble(10), rs.getDouble(11), rs.getDouble(12), rs.getDouble(13), rs.getString(14)));
                 }
             } catch (SQLException e) {
                 throw e;
